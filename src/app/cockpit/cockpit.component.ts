ee17c3aeabc5da10a3f673043bad41170337ae92
Samuel Pedro <samuelcpedro@gmail.com>
2018-11-27 22:06:37 -0100

    Binding to Custom Events

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
+
+  // properties
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output() blueprintCreated = new EventEmitter<{ blueprintName: string, blueprintContent: string }>();
 
   newServerName = '';
   newServerContent = '';
@@ -15,8 +19,12 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
 
-
   onAddServer() {
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
+
     // this.serverElements.push({
     //   type: 'server',
     //   name: this.newServerName,
@@ -25,6 +33,11 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddBlueprint() {
+    this.blueprintCreated.emit({
+      blueprintName: this.newServerName,
+      blueprintContent: this.newServerContent
+    });
+
     // this.serverElements.push({
     //   type: 'blueprint',
     //   name: this.newServerName,
