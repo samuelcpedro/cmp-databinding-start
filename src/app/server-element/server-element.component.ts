13d73ab093fe7192987af728977ab2bf246abeaf
Samuel Pedro <samuelcpedro@gmail.com>
2018-11-26 23:59:37 -0100

    alias in custom property binding
    https://angular.io/guide/styleguide#style-05-13

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
-  @Input() element: { type: string, name: string, content: string };
+  // tslint:disable-next-line:no-input-rename
+  @Input('srvElement') element: { type: string, name: string, content: string };
 
   constructor() { }
 
