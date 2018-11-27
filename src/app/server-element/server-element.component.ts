e65ef73d065587afa37951cd90462f9d1a158ddb
Samuel Pedro <samuelcpedro@gmail.com>
2018-11-27 22:28:10 -0100

    More on View Encapsulation
    More on View Encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,10 +1,15 @@
 import { element } from 'protractor';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // encapsulation: ViewEncapsulation.None // makes css from this component as gloval
+  // encapsulation: ViewEncapsulation.Native // uses the shadow DOM is compatble browsers
+  // For the DOM this means using modern Shadow DOM and creating a ShadowRoot for Component's Host Element.
+  // encapsulation: ViewEncapsulation.ShadowDom
+  encapsulation: ViewEncapsulation.Emulated // is the default
 })
 export class ServerElementComponent implements OnInit {
 
